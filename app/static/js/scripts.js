7b341cc06b59c304bb1dea8422c00d696df32a00
honehaniwa <trttkm@icloud.com>
2020-04-09 20:55:32 +0900

    button disable

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,10 +1,24 @@
 var infected = 0;
-function Update(){
-  document.getElementById("counter").value = num.toString(infected);
-}
-Update();
+// function Update(){
+//   document.getElementById("counter").value = num.toString(infected);
+// }
+// Update();
 function commonclick() {
-  window.alert(infected);
-    infected++;
-    Update();
+  // window.alert('ba');
+  console.log(infected++);
+  if(infected >=10){
+    document.getElementById('bat').disabled=false;
   }
+  // Update();
+}
+function batclick(){
+  if(infected>=10){
+    console.log('loop');
+    var bat=function(){
+      infected++;
+      console.log(infected);
+    }
+    setInterval(bat,1000);
+  }
+  // Update();
+}
